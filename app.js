11adf8d48091d12e629c71e360f9d58e835b7b1a
tsimeonov <tsimeonov12@gmail.com>
2024-05-19 12:34:09 +0200

    write the js for the boxes

---
+++ app.js
@@ -0,0 +1,7 @@
+const boxContainer = document.querySelector(".boxContainer");
+
+for (let i = 0; i < 365; i++) {
+  const el = document.createElement("div");
+  el.classList = "box";
+  boxContainer.appendChild(el);
+}
